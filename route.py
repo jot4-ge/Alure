ba703a0a957110e57240005b85b8c5c984625be7
TiagoUNB <tiago.geovaneunb@gmail.com>
2025-06-12 12:54:09 -0300

    fix: Fez com que se o usuario tentasse acessaer uma pagina que não existisse ele é redirecionado para a pagina inicial

--- route.py
+++ route.py
@@ -1,6 +1,7 @@
 from app.controllers.application import Application
 from bottle import Bottle, route, run, request, static_file
 from bottle import redirect, template, response, TEMPLATE_PATH
+from bottle import error, HTTPResponse
 import os
 
 # Configura o caminho dos templates
@@ -32,6 +33,14 @@ def initial_page(info=None):
     return ctl.render("initial_page")
 
 #-----------------------------------------------------------------------------
+# Se tentar abrir uma pagina que não existe redireciona para a pagina inicial
+
+@app.error(404)
+def error404(error_):
+    print(f"Erro = {error_}")
+    return HTTPResponse(status=302, headers={'Location': '/initial_page'})
+#-----------------------------------------------------------------------------
+
 # Inicialização do servidor
 if __name__ == '__main__':
     run(app, host='0.0.0.0', port=8080, debug=True)
