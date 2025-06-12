c9ef436916a106887dca61bfd346c85330e46d13
João Guilherme Amancio <joaoguiam@gmail.com>
2025-06-11 22:02:41 -0300

    Update application.py

--- app/controllers/application.py
+++ app/controllers/application.py
@@ -1,22 +1,21 @@
 from bottle import template
 
-
 class Application:
 
     def __init__(self):
         self.pages = {
-            "initial_page" : self.initial_page
+            "initial_page": self.initial_page,
+            "helper": self.helper
         }
 
-
-    def render(self,page):
-       content = self.pages.get(page, self.helper)
-       return content()
-
+    def render(self, page):
+        content = self.pages.get(page, self.helper)
+        return content()
 
     @staticmethod
     def helper():
-        return template('app/views/html/helper')
+        return template("helper")
+
     @staticmethod
     def initial_page():
-        return template("app/views/html/initial_page")
+        return template("initial_page")
